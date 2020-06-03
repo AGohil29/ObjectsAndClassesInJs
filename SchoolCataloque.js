fba62d420df3beba8ea1d25faabe8dd0ab8498c0
ArunGohil <arun.r@hungama.com>
2020-06-03 13:31:08 +0530

    school catalque project done

---
+++ SchoolCataloque.js
@@ -0,0 +1,65 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+
+    quickFacts() {
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+    }
+
+    static pickSubstituteTeacher(substituteTeachers) {
+        const randomNumber = Math.floor(Math.random() * substituteTeachers.length);
+        return substituteTeachers[randomNumber];
+    }
+
+    set numberOfStudents(newNumberOfStudents) {
+        if(typeof newNumberOfStudents === 'number'){
+            this._numberOfStudents = newNumberOfStudents;
+        } else {
+            console.log('Invalid input: numberOfStudents must be set to a Number.');
+        }
+    }
+}
+
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickUpPolicy) {
+        super(name, 'Primary', numberOfStudents);
+        this._pickUpPolicy = pickUpPolicy;
+    }
+
+    get pickUpPolicy() {
+        return this._pickUpPolicy;
+    }
+}
+
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeam) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeam = sportsTeam;
+    }
+
+    get sportsTeam() {
+        return console.log(this._sportsTeam.join(' '));
+    }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.sportsTeam;
